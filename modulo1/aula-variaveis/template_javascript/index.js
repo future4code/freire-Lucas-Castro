1f63c4b25fcfa5efad69a884cb333bd05b36745e
lucasteles <luvite@gmail.com>
2022-05-09 16:15:48 -0300

    Projeto da aula-variaveis e demais correções

---
+++ modulo1/aula-variaveis/template_javascript/index.js
@@ -0,0 +1,41 @@
+//1.Será impresso 10 10 5
+//2.Será impresso 10 10 10
+//3.p = horasPorDia, t = pagamentoPorDia. O Programa divide o seu ganho diário pela quantidade de horas trabalhadas e fornece o ganho por hora.
+
+/*let nome;
+let idade;
+
+console.log (typeof nome,idade) //Recebemos undefined porque não defimos valores para as variáveis.
+
+/*nome = prompt ("Digite seu nome")
+idade = prompt ("Digite sua idade")
+console.log ("Olá",nome, ",você tem" ,idade, "anos.")*/
+
+/*let a
+let b
+let c
+
+a = prompt("Você está feliz hoje?")
+b = prompt("Você é você?")
+c = prompt("Você namora?")
+
+console.log("Você está feliz hoje?",a)
+console.log("Você é você?",b)
+console.log("Você namora?",c)*/
+
+let a = 10
+let b = 25
+let trocar
+
+trocar = a
+a = b
+b = trocar
+
+console.log("O novo valor de a é:",a)
+console.log("O novo valor de b é:",b)
+
+
+
+
+
+
