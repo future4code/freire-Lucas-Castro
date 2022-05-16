0e23b1ff3a8cc5c432926d0012a46b835a81f0ab
lucasteles <luvite@gmail.com>
2022-05-16 16:42:17 -0300

    Adicionando arquivos da aula-objetos

---
+++ modulo1/aula-objetos/index.js
@@ -0,0 +1,85 @@
+// 1.A)Matheus Nachtergaele,Virginia Cavendish,canal: "Globo", horario: "14h"
+// 2.A)
+// nome: "Juca", 
+// idade: 3, 
+// raca: "SRD"
+
+// nome: "Juba", 
+// 	idade: 3, 
+// 	raca: "SRD"
+
+// nome: "Juco", 
+// 	idade: 3, 
+// 	raca: "SRD"
+
+// B)Copia as informações de outro objeto.
+
+// 3.A)False e undefined
+
+//   B)O valor impresso signica que temos false para backender e que a variável altura não foi definida.
+
+
+// Exercício 1 
+//const pessoa = {
+//     nome: "Lucas",
+//     apelidos: ["Lu","Luvite","Luvas"],
+// }
+
+// function funcaoPessoa(pessoa) {
+//     console.log(`Eu sou ${pessoa.nome} mas pode me chamar : ${pessoa.apelidos}`);
+// }
+// funcaoPessoa(pessoa)
+
+// const novoObjeto = {
+//     ...pessoa,
+//     apelidos: ["L","LULU","Luva"]
+// }
+// funcaoPessoa(novoObjeto)
+
+// Exercício 2
+//     const objeto1 = {
+//     Nome: "Lucas",
+//     Idade: "28",
+//     Profissao: "Programador",
+// }
+// const objeto2 = {
+//     Nome: "David",
+//     Idade: "27",
+//     Profissao: "Dançarino",
+//  }
+
+//  function pessoa(caracteristicas) {
+//      console.log (caracteristicas.Nome,caracteristicas.Nome.length,caracteristicas.Idade,caracteristicas.Profissao,caracteristicas.Profissao.length)
+//  }
+//  pessoa(objeto1)
+//  pessoa(objeto2)
+
+// Exercício 3
+// let carrinho = []
+// const fruta1 ={
+//     nome: "uva",
+//     disponibilidade: true,
+// }
+
+// const fruta2 ={
+//     nome: "goiaba",
+//     disponibilidade: true,
+// }
+
+// const fruta3 ={
+//     nome: "uva",
+//     disponibilidade: true,
+// }
+
+// function frutas(u)  {
+    
+//     return carrinho.push(u)
+// }
+// frutas(fruta1)
+// frutas(fruta2)
+// frutas(fruta3)
+// console.log(carrinho)
+
+
+
+
